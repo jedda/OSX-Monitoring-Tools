d1dd51e360d0ca5fb4e5e01c9b6be95267ef9f08
Jedda Wignall <jedda@jedda.me>
2013-01-07 12:43:33 +1100

    v1.1 - Added check for root user.

--- check_od_bdb.sh
+++ check_od_bdb.sh
@@ -4,11 +4,21 @@
 #	by Jedda Wignall
 #	http://jedda.me
 
+#	v1.1 - 07 Jan 2013
+#	Added check for root user.
+
 #	v1.0 - 27 Mar 2012
 #	Initial release.
 
 #	This script verifies each of the .bdb files in /var/db/openldap/openldap-data/*.bdb and returns
 #	a critical status if verification fails. You really only need to have this run once a day, or even weekly.
+
+#	This script must run with root privilege.
+
+if [[ $EUID -ne 0 ]]; then
+   printf "ERROR - This script must be run as root.\n"
+   exit 1
+fi
 
 for db in /var/db/openldap/openldap-data/*.bdb
 do
@@ -19,5 +29,5 @@ do
 	fi
 done
 
-printf "OK - All bdb databases verified\n"
+printf "OK - All bdb databases verified.\n"
 exit 0
