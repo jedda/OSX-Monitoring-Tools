10e4576b51291f8dbc9c053759f4f8818a632f14
Dan Barrett <dan@theblahman.net>
2013-11-04 22:22:06 +1100

    Added a check to see if Caching Server is running on a specific port

--- check_osx_caching.sh
+++ check_osx_caching.sh
@@ -4,6 +4,8 @@
 #	by Jedda Wignall
 #	http://jedda.me
 
+#   v1.2 - 04 Nov 2013
+#   Added specific port checking
 #	v1.1 - 17 Mar 2013
 #	Fixed quotation marks in databasePath (line 72) so that custom paths with spacing will work.
 #	v1.0 - 17 Dec 2012
@@ -12,35 +14,47 @@
 #	Script that uses serveradmin to check that the OS X Caching service is listed as running.
 #	If all is OK, it returns performance data for the size and limits of cached content, number of cached packages and
 #	bytes requested by and returned from the service.
+#   If you want to check to see if the Caching service is running on a specific port, use the -p flag and specify the port.
 
 #	Example:
 #	./check_osx_caching.sh
+#    OR
+#   ./check_osx_caching.sh -p 69010
 
 #	Performance Data - this script returns the followng Nagios performance data:
-#	reservedSpace -		Space reserved by the caching service for cache use.
-#	cacheUsed -       Space currently used by all cached content.
-#	cacheFree -       Space available for new cached content.
-#	cacheLimit -      User defined limit for cache in Server.app. If unlimited, free space on the selected volume.
-#	bytesRequested -	Bytes requested FROM APPLE for content. (Downloads from Apple's servers).
-#	bytesReturned -		Bytes returned TO CLIENTS for content. (Cached content, as well as real-time downloads from Apple's servers).
-#	numberOfPkgs -		Number of packages currently cached by the service.
+#	reservedSpace -       Space reserved by the caching service for cache use.
+#	cacheUsed -           Space currently used by all cached content.
+#	cacheFree -           Space available for new cached content.
+#	cacheLimit -          User defined limit for cache in Server.app. If unlimited, free space on the selected volume.
+#	bytesRequested -      Bytes requested FROM APPLE for content. (Downloads from Apple's servers).
+#	bytesReturned -       Bytes returned TO CLIENTS for content. (Cached content, as well as real-time downloads from Apple's servers).
+#	numberOfPkgs -        Number of packages currently cached by the service.
 
-# Mavericks Performance Data:
-# macAppsUsage -  Space used by App Store downloads
-# iosAppsUsage -  Space used by iOS apps and updates
-# ibooksUsage -   Space used by iBooks downloads
-# moviesUsage -   Space used by Movies downloads
-# musicUsage -    Space used by Music downloads
-# otherUsage -    Space used by Misc downloads
+#   Mavericks Performance Data:
+#   macAppsUsage -      Space used by App Store downloads
+#   iosAppsUsage -      Space used by iOS apps and updates
+#   ibooksUsage -       Space used by iBooks downloads
+#   moviesUsage -       Space used by Movies downloads
+#   musicUsage -        Space used by Music downloads
+#   otherUsage -        Space used by Misc downloads
 
 #	Compatibility - this script has been tested on and functions on the following stock OSes:
 #	10.8.2+ Server with Server.app 2.2+
-# 10.9+ Server with Server.app 3+
+#   10.9+ Server with Server.app 3+
 
 if [[ $EUID -ne 0 ]]; then
    printf "ERROR - This script must be run as root.\n"
    exit 1
 fi
+
+specificPort="0"
+
+while getopts "p:" opt
+  do
+    case $opt in
+      p ) specificPort=$OPTARG;;
+    esac
+done
 
 # Check that the caching service is running
 cachingStatus=`serveradmin fullstatus caching | grep 'caching:state' | sed -E 's/caching:state.+"(.+)"/\1/'`
@@ -65,6 +79,13 @@ if [ "$cachingActive" != "yes" ] || [ "$cachingStartupStatus" != "OK" ]; then
     exit 1
 fi
 
+cachingPort=`serveradmin fullstatus caching | grep 'caching:Port ' | grep -E -o "[0-9]+$"`
+if [ "$cachingPort" != "$specificPort" ]
+then
+    printf "WARNING - Caching Server is running on port $cachingPort and not $specificPort as required.\n"
+    exit 1
+fi
+
 # Check that the cache itself reports as OK
 cachingStatus=`serveradmin fullstatus caching | grep 'caching:CacheStatus' | sed -E 's/caching:CacheStatus.+"(.+)"/\1/'`
 if [ "$cachingStatus" != "OK" ]; then
@@ -79,14 +100,14 @@ mavericksPerfData=''
 
 if [ $isMavericks -eq 0 ]
 then
-  macAppsUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:0:BytesUsed' | grep -E -o "[0-9]+$"`
-  iosAppsUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:1:BytesUsed' | grep -E -o "[0-9]+$"`
-  ibooksUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:2:BytesUsed' | grep -E -o "[0-9]+$"`
-  moviesUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:3:BytesUsed' | grep -E -o "[0-9]+$"`
-  musicUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:4:BytesUsed' | grep -E -o "[0-9]+$"`
-  otherUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:5:BytesUsed' | grep -E -o "[0-9]+$"`
+    macAppsUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:0:BytesUsed' | grep -E -o "[0-9]+$"`
+    iosAppsUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:1:BytesUsed' | grep -E -o "[0-9]+$"`
+    ibooksUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:2:BytesUsed' | grep -E -o "[0-9]+$"`
+    moviesUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:3:BytesUsed' | grep -E -o "[0-9]+$"`
+    musicUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:4:BytesUsed' | grep -E -o "[0-9]+$"`
+    otherUsage=`serveradmin fullstatus caching | grep 'caching:CacheDetails:_array_index:5:BytesUsed' | grep -E -o "[0-9]+$"`
 
-  mavericksPerfData="macAppsUsage=$macAppsUsage; iosAppsUsage=$iosAppsUsage; ibooksUsage=$ibooksUsage; moviesUsage=$moviesUsage; musicUsage=$musicUsage; otherUsage=$otherUsage;"
+    mavericksPerfData="macAppsUsage=$macAppsUsage; iosAppsUsage=$iosAppsUsage; ibooksUsage=$ibooksUsage; moviesUsage=$moviesUsage; musicUsage=$musicUsage; otherUsage=$otherUsage;"
 fi
 
 # Grab our performance data
