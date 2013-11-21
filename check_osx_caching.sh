d22af15e4fe4ce542473af60d963de3584894f14
Dan Barrett <dan@theblahman.net>
2013-11-22 08:12:16 +1100

    Minor tab formatting changes

--- check_osx_caching.sh
+++ check_osx_caching.sh
@@ -1,34 +1,36 @@
 #!/bin/bash
 
-#	Check OS X Caching Server
-#	by Jedda Wignall
-#	http://jedda.me
+#   Check OS X Caching Server
+#   by Jedda Wignall
+#   http://jedda.me
 
 #   v1.2 - 04 Nov 2013
 #   Added specific port checking
-#	v1.1 - 17 Mar 2013
-#	Fixed quotation marks in databasePath (line 72) so that custom paths with spacing will work.
-#	v1.0 - 17 Dec 2012
-#	Initial release.
 
-#	Script that uses serveradmin to check that the OS X Caching service is listed as running.
-#	If all is OK, it returns performance data for the size and limits of cached content, number of cached packages and
-#	bytes requested by and returned from the service.
+#   v1.1 - 17 Mar 2013
+#   Fixed quotation marks in databasePath (line 72) so that custom paths with spacing will work.
+
+#   v1.0 - 17 Dec 2012
+#   Initial release.
+
+#   Script that uses serveradmin to check that the OS X Caching service is listed as running.
+#   If all is OK, it returns performance data for the size and limits of cached content, number of cached packages and
+#   bytes requested by and returned from the service.
 #   Also checks to see if the Caching service is running on the port that is set in caching:Port
 
-#	Example:
-#	./check_osx_caching.sh
+#   Example:
+#   ./check_osx_caching.sh
 
-#	Performance Data - this script returns the followng Nagios performance data:
-#	reservedSpace -       Space reserved by the caching service for cache use.
-#	cacheUsed -           Space currently used by all cached content.
-#	cacheFree -           Space available for new cached content.
-#	cacheLimit -          User defined limit for cache in Server.app. If unlimited, free space on the selected volume.
-#	bytesRequested -      Bytes requested FROM APPLE for content. (Downloads from Apple's servers).
-#	bytesReturned -       Bytes returned TO CLIENTS for content. (Cached content, as well as real-time downloads from Apple's servers).
-#	numberOfPkgs -        Number of packages currently cached by the service.
+#   Performance Data - this script returns the followng Nagios performance data:
+#   reservedSpace -       Space reserved by the caching service for cache use.
+#   cacheUsed -           Space currently used by all cached content.
+#   cacheFree -           Space available for new cached content.
+#   cacheLimit -          User defined limit for cache in Server.app. If unlimited, free space on the selected volume.
+#   bytesRequested -      Bytes requested FROM APPLE for content. (Downloads from Apple's servers).
+#   bytesReturned -       Bytes returned TO CLIENTS for content. (Cached content, as well as real-time downloads from Apple's servers).
+#   numberOfPkgs -        Number of packages currently cached by the service.
 
-#   Mavericks Performance Data:
+#   Additional Mavericks Performance Data:
 #   macAppsUsage -      Space used by App Store downloads
 #   iosAppsUsage -      Space used by iOS apps and updates
 #   ibooksUsage -       Space used by iBooks downloads
@@ -36,26 +38,26 @@
 #   musicUsage -        Space used by Music downloads
 #   otherUsage -        Space used by Misc downloads
 
-#	Compatibility - this script has been tested on and functions on the following stock OSes:
-#	10.8.2+ Server with Server.app 2.2+
+#   Compatibility - this script has been tested on and functions on the following stock OSes:
+#   10.8.2+ Server with Server.app 2.2+
 #   10.9+ Server with Server.app 3+
 
 if [[ $EUID -ne 0 ]]; then
-   printf "ERROR - This script must be run as root.\n"
-   exit 1
+    printf "ERROR - This script must be run as root.\n"
+    exit 1
 fi
 
 # Check that the caching service is running
 cachingStatus=`serveradmin fullstatus caching | grep 'caching:state' | sed -E 's/caching:state.+"(.+)"/\1/'`
 if [ "$cachingStatus" != "RUNNING" ]; then
-	printf "CRITICAL - Caching service is not running!\n"
+    printf "CRITICAL - Caching service is not running!\n"
     exit 2
 fi
 
 # Check that the caching service has registered with Apple
 cachingRegistrationStatus=`serveradmin fullstatus caching | grep 'caching:RegistrationStatus ' | grep -E -o "[0-9]+$"`
 if [ "$cachingRegistrationStatus" != "1" ]; then
-	printf "CRITICAL - Caching service has not yet registered with Apple!\n"
+    printf "CRITICAL - Caching service has not yet registered with Apple!\n"
     exit 2
 fi
 
@@ -64,7 +66,7 @@ fi
 cachingActive=`serveradmin fullstatus caching | grep 'caching:Active ' | grep -E -o "[a-z]+$"`
 cachingStartupStatus=`serveradmin fullstatus caching | grep 'caching:StartupStatus' | sed -E 's/caching:StartupStatus.+"(.+)"/\1/'`
 if [ "$cachingActive" != "yes" ] || [ "$cachingStartupStatus" != "OK" ]; then
-	printf "WARNING - Caching service is running, but has not fully started up.\n"
+    printf "WARNING - Caching service is running, but has not fully started up.\n"
     exit 1
 fi
 
@@ -82,7 +84,7 @@ fi
 # Check that the cache itself reports as OK
 cachingStatus=`serveradmin fullstatus caching | grep 'caching:CacheStatus' | sed -E 's/caching:CacheStatus.+"(.+)"/\1/'`
 if [ "$cachingStatus" != "OK" ]; then
-	printf "WARNING - Caching service reported a problem with its data cache.\n"
+    printf "WARNING - Caching service reported a problem with its data cache.\n"
     exit 1
 fi
 
