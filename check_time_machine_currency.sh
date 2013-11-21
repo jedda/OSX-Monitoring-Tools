f4834328b3a9c3db54e708d66d28a376165fc067
Dan Barrett <dan@theblahman.net>
2013-11-21 22:18:18 +1100

    Added support for Time Machine in OS X Mavericks (10.9)

--- check_time_machine_currency.sh
+++ check_time_machine_currency.sh
@@ -1,64 +1,108 @@
 #!/bin/bash
 
-#       Check Time Machine Currency
-#       by Jedda Wignall
-#       http://jedda.me
+#   Check Time Machine Currency
+#   by Jedda Wignall
+#   http://jedda.me
 
-#       v1.1 - 3 May 2012
-#       Cleaned up the output to provide a last backed up date. Error checking for non-supplied flags.
+#   Additional Code by:
+#   Dan Barrett
+#   http://yesdevnull.net
 
-#       v1.0 - 3 May 2012
-#       Initial release.
+#   v1.2 - 21 Nov 2013
+#   Updated with support for OS X Mavericks (10.9)
 
-#       This script checks the time machine results file on a Mac, and reports if a backup has completed within a number of minutes of the current time.
-#       Takes two arguments (warning threshold in minutes, critical threshold in minutes):
-#       ./check_time_machine_currency.sh -w 240 -c 1440
+#   v1.1 - 3 May 2012
+#   Cleaned up the output to provide a last backed up date. Error checking for non-supplied flags.
 
-#       Very useful if you are monitoring client production systems, and want to ensure backups are occurring.
+#   v1.0 - 3 May 2012
+#   Initial release.
 
-while getopts "d:w:c:" optionName; do
-case "$optionName" in
-w) warnMinutes=( $OPTARG );;
-c) critMinutes=( $OPTARG );;
-esac
+#   This script checks the Time Machine results file on a Mac, and reports if a backup has completed within a number of minutes of the current time.
+#   Very useful if you are monitoring client production systems, and want to ensure backups are occurring.
+
+#   Arguments:
+#   -w     Warning threshold in minutes
+#   -c     Critical threshold in minutes
+
+#   Example:
+#   ./check_time_machine_currency.sh -w 240 -c 1440
+
+#   Supports:
+#   * OS X 10.6+
+#   * OS X 10.7+
+#   * OS X 10.8+
+#   * OS X 10.9
+
+while getopts "w:c:" opt
+    do
+        case $opt in
+                w ) warnMinutes=$OPTARG;;
+                c ) critMinutes=$OPTARG;;
+        esac
 done
 
-if [ "$warnMinutes" == "" ]; then
-        printf "ERROR - You must provide a warning threshold with -w!\n"
-        exit 3
+if [ "$warnMinutes" == "" ]
+then
+    printf "ERROR - You must provide a warning threshold with -w!\n"
+    exit 3
 fi
 
-if [ "$critMinutes" == "" ]; then
-        printf "ERROR - You must provide a critical threshold with -c!\n"
-        exit 3
+if [ "$critMinutes" == "" ]
+then
+    printf "ERROR - You must provide a critical threshold with -c!\n"
+    exit 3
 fi
 
-lastBackupDateString=`defaults read /private/var/db/.TimeMachine.Results BACKUP_COMPLETED_DATE`
+# Check to see if we're running Mavericks as Time Machine runs a little differently
+osVersion=`sw_vers -productVersion | grep -E -o "[0-9]+\.[0-9]"`
+isMavericks=`echo $osVersion '< 10.9' | bc -l`
 
-if [ "$lastBackupDateString" == "" ]; then
+if [ $isMavericks -eq 0 ]
+then
+    # 10.9+ Check
+    lastBackupDateString=`tmutil latestbackup | grep -E -o "[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{6}"`
+
+    if [ "$lastBackupDateString" == "" ]
+    then
+            printf "CRITICAL - Time Machine has not completed a backup on this Mac!\n"
+            exit 2
+    fi
+
+    lastBackupDate=`date -j -f "%Y-%m-%d-%H%M%S" $lastBackupDateString "+%s"`
+else
+    # < 10.9 Check
+    lastBackupDateString=`defaults read /private/var/db/.TimeMachine.Results BACKUP_COMPLETED_DATE`
+
+    if [ "$lastBackupDateString" == "" ]
+    then
         printf "CRITICAL - Time Machine has not completed a backup on this Mac!\n"
         exit 2
+    fi
+
+    lastBackupDate=`date -j -f "%Y-%m-%e %H:%M:%S %z" "$lastBackupDateString" "+%s"`
 fi
 
-lastBackupDate=$(date -j -f "%Y-%m-%e %H:%M:%S %z" "$lastBackupDateString" "+%s" )
-currentDate=$(date +%s)
+currentDate=`date +%s`
 
-diff=$(( $currentDate - $lastBackupDate))
+diff=$(($currentDate - $lastBackupDate))
 warnSeconds=$(($warnMinutes * 60))
 critSeconds=$(($critMinutes * 60))
 
-if [ "$diff" -gt "$critSeconds" ]; then
-        printf "CRITICAL - Time Machine has not backed up since `date -j -f %s $lastBackupDate` (more than $critMinutes minutes)!\n"
-        exit 2
-elif [ "$diff" -gt "$warnSeconds" ]; then
-        printf "WARNING - Time Machine has not backed up since `date -j -f %s $lastBackupDate` (more than $warnMinutes minutes)!\n"
-        exit 1
+if [ "$diff" -gt "$critSeconds" ]
+then
+    printf "CRITICAL - Time Machine has not backed up since `date -j -f %s $lastBackupDate` (more than $critMinutes minutes)!\n"
+    exit 2
+elif [ "$diff" -gt "$warnSeconds" ]
+then
+    printf "WARNING - Time Machine has not backed up since `date -j -f %s $lastBackupDate` (more than $warnMinutes minutes)!\n"
+    exit 1
 fi
 
-if [ "$lastBackupDate" != "" ]; then
-        printf "OK - A Time Machine backup has been taken within the last $warnMinutes minutes.\n"
-        exit 0
+if [ "$lastBackupDate" != "" ]
+then
+    printf "OK - A Time Machine backup has been taken within the last $warnMinutes minutes.\n"
+    exit 0
 else
-        printf "CRITICAL - Could not determine the last backup date for this Mac."
-        exit 2
+    printf "CRITICAL - Could not determine the last backup date for this Mac.\n"
+    exit 2
 fi
