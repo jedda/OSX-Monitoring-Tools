deec911e553b8591adfb32b90de39252da3708df
Dan Barrett <dan@theblahman.net>
2013-11-29 18:16:27 +1100

    Added start verification date checking; minor code changes.

--- check_ssl_certificate_expiry.sh
+++ check_ssl_certificate_expiry.sh
@@ -4,7 +4,7 @@
 #	by Dan Barrett
 #	http://yesdevnull.net
 
-#	v1.0 - 28 November 2013
+#	v1.0 - 29 November 2013
 #	Initial release.
 
 #	Checks the specified certificate and warns you if the certificate is going
@@ -12,12 +12,13 @@
 
 #	Arguments:
 #	-h   Host address
-#	-p   Port o
+#	-p   Port of SSL service
 #	-e   Expiry in days
 
 #	Example:
-#	./check_ssl_certificate.expiry -h google.com -p 443 -e 7
+#	./check_ssl_certificate.expiry -h apple.com -p 443 -e 7
 
+# Set up our blank variables
 host=""
 port=""
 expiryInDays=""
@@ -49,38 +50,47 @@ numberOfDays() {
 	echo "$dayDiff $dayName"
 }
 
-beforeExpiryRequest=`echo "QUIT" | openssl s_client -connect $host:$port 2>/dev/null | openssl x509 -noout -startdate 1> /tmp/cert_before_output`
-beforeExpiryResult=$(</tmp/cert_before_output)
-afterExpiryRequest=`echo "QUIT" | openssl s_client -connect $host:$port 2>/dev/null | openssl x509 -noout -enddate 1> /tmp/cert_after_output`
-afterExpiryResult=$(</tmp/cert_after_output)
+beforeExpiry=`echo "QUIT" | openssl s_client -connect $host:$port 2>/dev/null | openssl x509 -noout -startdate 2>/dev/null`
+afterExpiry=`echo "QUIT" | openssl s_client -connect $host:$port 2>/dev/null | openssl x509 -noout -enddate 2>/dev/null`
 
-notBefore=`echo $beforeExpiryResult | grep -C 0 "notBefore" | grep -E -o "[A-Za-z]{3,4} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{3,4} [A-Z]{2,3}"`
+# If the stdout of the date results is null, throw a critical(2)
+if [ -z "$beforeExpiry" ] || [ -z "$afterExpiry" ]
+then
+	printf "CRITICAL - Unable to read certificate.\n"
+	exit 2
+fi
+
+notBefore=`echo $beforeExpiry | grep -C 0 "notBefore" | grep -E -o "[A-Za-z]{3,4} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{3,4} [A-Z]{2,3}"`
 notBeforeExpiry=`date -j -f "%b %d %H:%M:%S %Y %Z" "$notBefore" "+%s"`
 
 diff=$(( $currentDateInEpoch - $notBeforeExpiry ))
 
+# Is certificate not valid until the future?  If so, throw a critical(2)
 if [ "$diff" -lt "0" ]
 then
 	printf "CRITICAL - Certificate is not valid until $( numberOfDays $diff | cut -c 2- )!\n"
 	exit 2
 fi
 
-notAfter=`echo $afterExpiryResult | grep -C 0 "notAfter" | grep -E -o "[A-Za-z]{3,4} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{3,4} [A-Z]{2,3}"`
+notAfter=`echo $afterExpiry | grep -C 0 "notAfter" | grep -E -o "[A-Za-z]{3,4} [0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} [0-9]{3,4} [A-Z]{2,3}"`
 notAfterExpiry=`date -j -f "%b %d %H:%M:%S %Y %Z" "$notAfter" "+%s"`
 
 diff=$(( $notAfterExpiry - $currentDateInEpoch ))
 
+# If the differential is less than 0, the certificate has already expired, throw a critical(2)
 if [ "$diff" -lt "0" ]
 then
 	printf "CRITICAL - Certificate expired $( numberOfDays $diff | cut -c 2- ) ago!\n"
 	exit 2
 fi
 
+# If the differential is less than the expiry days, throw a warning(1)
 if [ "$diff" -lt "$expiryDays" ]
 then
 	printf "WARNING - Certificate will expire in less than $( numberOfDays $diff ).\n"
 	exit 1
 fi
 
+# All OK(0)
 printf "OK - Certificate expires in $( numberOfDays $diff ).\n"
 exit 0
